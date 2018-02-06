c4143e6f220054f7a8b80df5991f42f7bddcd927
Brandon Renfrow <bjrenfrow@gmail.com>
2018-02-05 23:51:54 -0800

    WIP: progress

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-console.log('hello');
+const axios = require('axios');
+const moment = require('moment');
+const program = require('commander');
+const R = require('ramda');
+
+const parseUSDHistory = R.path(['data', 'price_usd']);
+
+const coinmarketApi = (start, end) => 
+  `https://graphs2.coinmarketcap.com/currencies/breakout/${start}/${end}/`;
+
+const getTimeframeFromYear = R.compose(
+  year => ({
+    end: year.endOf('year').valueOf(),
+    start: year.valueOf(),
+  }),
+  year => moment(`${program.year}-01-01`),
+);
+
+console.log('-- starting --');
+
+(async () => {
+  program
+    .version('1.0.0')
+    .option('-f, --file <file>', 'breakout csv')
+    .option('-y, --year <year>', 'tax year')
+    .parse(process.argv);
+    
+  const { end, start } = getTimeframeFromYear(program.year)
+    
+  // const results = await axios.get(coinmarketApi(1508068553444, 1517900050000));
+  // const history = parseUSDHistory(results);
+  
+  // console.log({ history });
+    
+})();
